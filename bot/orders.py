f81896536d5e871cf53e9806392f401c3eb609ba
anjalinandwl-eng <anjalinandwl@gmail.com>
2026-06-12 15:25:00 +0530

    Create orders.py

---
+++ bot/orders.py
@@ -0,0 +1,27 @@
+from client import client
+
+
+def place_market_order(symbol, side, quantity):
+
+    order = client.futures_create_order(
+        symbol=symbol,
+        side=side,
+        type="MARKET",
+        quantity=quantity
+    )
+
+    return order
+
+
+def place_limit_order(symbol, side, quantity, price):
+
+    order = client.futures_create_order(
+        symbol=symbol,
+        side=side,
+        type="LIMIT",
+        quantity=quantity,
+        price=price,
+        timeInForce="GTC"
+    )
+
+    return order
